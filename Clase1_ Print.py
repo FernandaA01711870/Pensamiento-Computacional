69f0e2747ab6f32b5e8eee8344e6a6666bf80381
FernandaA01711870 <142357001+FernandaA01711870@users.noreply.github.com>
2023-08-15 16:44:03 -0600

    Add files via upload

---
+++ Clase1_ Print.py
@@ -0,0 +1,13 @@
+#print("Tecnologico de Monterrey\n")
+#print("Bienvenido al curso de Pensamiento Computacional para Inegenieria")
+#print("\tAtentamente: Los Profesores")
+
+print("Tecnologico de Monterrey\n\nBienvenido al curso de Pensamiento Computacional para ingenieria\n\n \tAtentamente: Los Profesores")#Regla de convención: las lineas no deben de ser mayores a 70 caracteres
+
+#Los dos procedimientos son correctos, uno más efeciente que otro
+
+"""
+kknsdkasnkasnfakna
+
+Otra formna de comentar cosas más largas es usando tres comillas dobles
+"""
