b129048352219799f06eb6a673b60978ff8e46be
FernandaA01711870 <142357001+FernandaA01711870@users.noreply.github.com>
2023-09-01 23:51:58 -0600

    Update proyecto.py
    Se declararon variables para perímetros y áreas y se mejoro el ciclo while (ahora permite navegar entre los diferentes menús).

--- proyecto.py
+++ proyecto.py
@@ -1,203 +1,209 @@
-#Mostrar un mensaje de bienvenida al usuario
+Importar la libreria math
+import math
 
-print("Bienvenido a su calculadora de áreas y perímetros\n")
 
-print("Indique si quiere calcular perimetros o áreas: \n") 
+#Declarar las funciones de perimetro que vamos a utilizar
+def perimetro_cuadrado (a):
+  return a*4
 
-"""En esta parte de deslegara un menu por el cual el usuario podra escoger entre calcular perímetro o area de una figura"""
+def perimetro_triangulo (a,b,c): 
+  return a + b + c
 
-menu_principal = int(input("1- Perimetros\n2- Áreas\n3- Salir\n\n"))
+def perimetro_rectangulo (a, b):
+  return 2 * (b + a)
 
-while menu_principal != 3:
+def perimetro_paralelogramo (a, b):
+  return 2 * (b +a)
+
+def perimetro_rombo (a):
+  return a*4
+
+def perimetro_trapecio (B, b, a, c):
+  return B + b + a + c
+
+def perimetro_circulo (r):
+  return 2 * math.pi * r
+
+def perimetro_poligono (n,b):
+  return n*b
+
+#Declarar las funciones de áreas que vamos a utilizar
+def area_cuadrado (a):
+   return a**2
+
+def area_triangulo (b, h): 
+  return (b * h) / 2
+
+def area_rectangulo (b, a):
+  return  b * a
+
+def area_paralelogramo (b, h):
+  return b * h
+
+def area_rombo (D, d):
+  return (D*d)/2
+
+def area_trapecio (B, d, h):
+  return ((B + d)*h)/2
+
+def area_circulo (r):
+  return math.pi * r**2
+
+def area_poligono (n, b, a):
+  return (n*b)*a/2
+
+#Crear un cilo While para desplegar una serie de menús con distintas opciones a elejir
+
+#Menú principal
+
+menu_principal = 0
+
+while True:
+
+  print("Bienvenido a su calculadora de áreas y perímetros\n")
+
+  print("Indique si quiere calcular perimetros o áreas: \n") 
   
-  if menu_principal == 1:  #Calcular perimetros
-    print("Bienvenido al menú de perimetros\n")
-    print("Indique de que figura le gustaria conocer su perimetro\n")
-
-    menu_perimetro = int(input("1- Cuadrado\n2- Triangulo\n3- Rectangulo\n4- Paralelogramo\n5- Rombo \n6- Trapecio\n7- Circulo\n8- Poligono Regular\n9- Salir\n"))
-
-    while menu_perimetro != 9:
+  menu_principal = int(input("1- Perimetros\n2- Áreas\n3- Salir\n\n"))
+  
+#Menú de perimetros
+  
+  if menu_principal == 1: 
     
-      if menu_perimetro == 1:
-       a = float(input("Ingrese un lado del cuadrado: "))
-       p_cuadrado = a * 4
-       print("El perimetro del cuadrado es: ", p_cuadrado, "\n")
+    menu_perimetro = 0
 
-       break
+    while True:
+      
+      print("Bienvenido al menú de perimetros\n")
+      print("Indique de que figura le gustaria conocer su perimetro\n")
+      
+      menu_perimetro = int(input("1- Cuadrado\n2- Triangulo\n3-  Rectangulo\n4- Paralelogramo\n5- Rombo \n6- Trapecio\n7- Circulo\n8- Poligono Regular\n9- Regresar al menú principal\n"))
+      
+      if menu_perimetro == 1:
+       num1 = float(input("Ingrese un lado del cuadrado: "))
+        
+       print("El perimetro del cuadrado es: ", perimetro_cuadrado(num1), "\n")
         
       elif menu_perimetro == 2:
-        a = float(input("Ingrese un lado del triangulo: "))
-        b = float(input("Ingrese el segundo lado del triangulo: "))
-        c = float(input("Ingrese el tercer lado del triangulo :"))
+        num1 = float(input("Ingrese un lado del triangulo: "))
+        num2 = float(input("Ingrese el segundo lado del triangulo: "))
+        num3 = float(input("Ingrese el tercer lado del triangulo :"))
 
-        p_triangulo = a + b + c
-
-        print("El perimetro del triangulo es: ", p_triangulo, "\n")
-
-        break
+        print("El perimetro del trinagulo es: ", perimetro_triangulo(num1,num2,num3), "\n")
       
       elif menu_perimetro == 3:
-        a = float(input("Ingrese la altura del rectangulo: "))
-        b = float(input("Ingrese la base del rectangulo: "))
-        
-        p_rectangulo = 2*(b * a)
-        
-        print("El perimetro del rectangulo es: ", p_rectangulo, "\n")
+        num1 = float(input("Ingrese la altura del rectangulo: "))
+        num2 = float(input("Ingrese la base del rectangulo: "))
 
-        break
+        print("El perimetro del rectangulo es: ", perimetro_rectangulo(num1,num2), "\n")
         
       elif menu_perimetro == 4:
-        a = float(input("Ingrese la altura del paralelogramo: "))
-        b = float(input("Ingrese la base del paralelogramo: "))
-        
-        p_paralelogramo = 2*(b * a)
-        
-        print("El perimetro del paralelogramo es: ", p_paralelogramo, "\n")
+        num1 = float(input("Ingrese la altura del paralelogramo: "))
+        num2 = float(input("Ingrese la base del paralelogramo: "))
 
-        break
+        print("El perimetro del paralelogramo es: ", perimetro_paralelogramo(num1,num2), "\n")
         
       elif menu_perimetro == 5:
-        a = float(input("Ingrese un lado del rombo: "))
-        
-        p_rombo = 4*a
-        
-        print("El perimetro del rombo es: ", p_rombo, "\n")
+        num1 = float(input("Ingrese un lado del rombo: "))
 
-        break
+        print("El perimetro del rombo es: ", perimetro_rombo(num1), "\n")
         
       elif menu_perimetro == 6:
-        B = float(input("Ingrese la altura mayor del trapecio: "))
-        b = float(input("Ingrese la altura menor del trapecio: "))
-        a = float(input("Ingrese un lado del trapecio: "))
-        c = float(input("Ingrese el segundo lado del trapecio: "))
-        p_trapecio = B + b + a + c
-        
-        print("El perimetro del trapecio es: ", p_trapecio, "\n")
+        num1 = float(input("Ingrese la base mayor del trapecio: "))
+        num2 = float(input("Ingrese la base menor del trapecio: "))
+        num3 = float(input("Ingrese un lado del trapecio: "))
+        num4 = float(input("Ingrese el segundo lado del trapecio: "))
 
-        break
+        print("El perimetro del trapecio es: ", perimetro_trapecio(num1,num2,num3,num4), "\n")
       
       elif menu_perimetro == 7:
-        r = float(input("Ingrese el radio del circulo: "))
+        num1 = float(input("Ingrese el radio del circulo: "))
 
-        PI = 3.1416
-        p_circulo = 2 * PI * r
-        
-        print("El perimetro del circulo es: ", p_circulo, "\n")
-
-        break
+        print("El perimetro del circulo es: ", perimetro_circulo(num1), "\n")
         
       elif menu_perimetro == 8:
-        n = float(input("Ingrese el numero de lados del poligono: "))
-        b = float(input("Ingrese un lado del poligono: "))
-        
-        p_poligono = n*b
-        
-        print("El perimetro del poligono es: ", p_poligono, "\n")
-        
-        break
-        
-      else:
-        
-        print("Por favor digite una opción correcta")
+        num1 = float(input("Ingrese el numero de lados del poligono: "))
+        num2 = float(input("Ingrese un lado del poligono: "))
 
-      break
-      
-    break
-    
-  elif menu_principal == 2:
-    print("Bienvenido al menú de áreas\n")
-    print("Indique de que figura le gustaria conocer su área\n")
+        print("El perimetro del poligono es: ", perimetro_poligono(num1,num2), "\n")
 
-    menu_area = int(input("1- Cuadrado\n2- Triangulo\n3- Rectangulo\n4- Paralelogramo\n5- Rombo \n6- Trapecio\n7- Circulo\n8- Poligono Regular \n9- Salir \n\n"))
-
-    while menu_area != 9:
-
-      if menu_area == 1:
-       a = float(input("Ingrese un lado del cuadrado: "))
-       a_cuadrado = a ** 2
-       print("El area del cuadrado es: ", a_cuadrado, "\n")
-
+      elif menu_perimetro == 9:
        break
         
-      elif menu_area == 2:
-        b = float(input("Ingrese la base del triangulo: "))
-        h = float(input("Ingrese la altura del triangulo :"))
-
-        a_triangulo = b * h / 2
-
-        print("El area del triangulo es: ", a_triangulo, "\n")
-
-        break
-      
-      elif menu_area == 3:
-        a = float(input("Ingrese la altura del rectangulo: "))
-        b = float(input("Ingrese la base del rectangulo: "))
-        
-        a_rectangulo = b * a
-        
-        print("El area del rectangulo es: ", a_rectangulo, "\n")
-
-        break
-        
-      elif menu_area == 4:
-        h = float(input("Ingrese la altura (h) del paralelogramo: "))
-        b = float(input("Ingrese la base del paralelogramo: "))
-        
-        a_paralelogramo = b * h
-        
-        print("El perimetro del paralelogramo es: ", a_paralelogramo, "\n")
-
-        break
-        
-      elif menu_area == 5:
-        D = float(input("Ingrese la diagonal mayor del rombo: "))
-        d = float(input("Ingrese la diagonal menor del rombo: "))
-        
-        a_rombo = D*d/2
-        
-        print("El perimetro del rombo es: ", a_rombo, "\n")
-
-        break
-        
-      elif menu_area == 6:
-        B = float(input("Ingrese la base mayor : "))
-        b = float(input("Ingrese la base menor: "))
-        h = float(input("Ingrese la altura (h): "))
-        a_trapecio = (B * b)/2
-        
-        print("El perimetro del trapecio es: ", a_trapecio, "\n")
-
-        break
-    
-      elif menu_area == 7:
-        r = float(input("Ingrese el radio del circulo: "))
-        
-        PI = 3.1416
-        a_circulo = PI * r**2
-        
-        print("El perimetro del circulo es: ", a_circulo, "\n")
-
-        break
-        
-      elif menu_area == 8:
-        n = float(input("Ingrese el numero de lados del poligono: "))
-        b = float(input("Ingrese un lado del poligono: "))
-        a = float(input("Ingrese la apotema: "))
-        
-        a_poligono = (n*b)*a/2
-        
-        print("El perimetro del poligono es: ", a_poligono, "\n")
-        
-        break
-        
       else:
-        
         print("Por favor digite una opción correcta")
+        
+#Menú de areas
+  
+  elif menu_principal == 2:
 
-      break
+    menu_area = 0
+
+    while True:
       
+     print("Bienvenido al menú de áreas\n")
+     print("Indique de que figura le gustaria conocer su área\n")
+      
+     menu_area = int(input("1- Cuadrado\n2- Triangulo\n3- Rectangulo\n4- Paralelogramo\n5- Rombo \n6- Trapecio\n7- Circulo\n8- Poligono Regular \n9- Regresar al menú principal \n\n"))
+      
+     if menu_area == 1:
+       num1 = float(input("Ingrese un lado del cuadrado: "))
+
+       print("El área del cuadrado es: " , area_cuadrado(num1), "\n")
+        
+     elif menu_area == 2:
+        num1 = float(input("Ingrese la base del triangulo: "))
+        num2 = float(input("Ingrese la altura del triangulo :"))
+
+        print("El área del triangulo es: ", area_triangulo(num1,num2), "\n")
+      
+     elif menu_area == 3:
+        num1 = float(input("Ingrese la altura del rectangulo: "))
+        num2 = float(input("Ingrese la base del rectangulo: "))
+        
+        print("El area del rectangulo es: ", area_rectangulo(num1,num2), "\n")
+        
+     elif menu_area == 4:
+        num1 = float(input("Ingrese la altura (h) del paralelogramo: "))
+        num2 = float(input("Ingrese la base del paralelogramo: "))
+
+        print("El área del paralelogramo es: ", area_paralelogramo(num1,num2), "\n")
+        
+     elif menu_area == 5:
+        num1 = float(input("Ingrese la diagonal mayor del rombo: "))
+        num2 = float(input("Ingrese la diagonal menor del rombo: "))
+        
+        print("El area del rombo es: ", area_rombo(num1,num2), "\n")
+        
+     elif menu_area == 6:
+        num1 = float(input("Ingrese la base mayor del trapecio: "))
+        num2 = float(input("Ingrese la base menor del trapecio: "))
+        num3 = float(input("Ingrese la altura (h) del trapecio: "))
+        
+        print("El area del trapecio es: ", area_trapecio(num1,num2,num3), "\n")
+    
+     elif menu_area == 7:
+        num1 = float(input("Ingrese el radio del circulo: "))
+
+        print("El area del circulo es: ", area_circulo(num1), "\n")
+        
+     elif menu_area == 8:
+        num1 = float(input("Ingrese el numero de lados del poligono: "))
+        num2 = float(input("Ingrese un lado del poligono: "))
+        num3 = float(input("Ingrese la apotema: "))
+        
+        print("El area del poligono es: ", area_poligono(num1,num2,num3), "\n")
+      
+     elif menu_area == 9:
+      break
+    
+    else:
+        print("Por favor digite una opción correcta")
+  
+  elif menu_principal == 3:
     break
-     
+
+#Si ninguna opción se cumple mostar un mensaje de error
+  
   else:
-    print("Por favor digite una opción correcta")
-    break
+    print("Por favor digite una opción correcta \n")
